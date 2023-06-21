52af10074ab3fce646d25a5effc080fc2cd349b2
kaavya-25 <116670352+kaavya-25@users.noreply.github.com>
2023-06-21 17:54:56 +0530

    commit message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AgeCalculator from './component/age'
+
+function App() {
+  return (
+    <>
+<AgeCalculator/>
+    </>
+  );
+}
+
+export default App;
